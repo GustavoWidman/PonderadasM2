49b9906f59f69c5be4e3e58dd0d4986573016b4f
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-05-28 23:16:07 -0300

    Entrega atv programacao semana 6

---
+++ SEMANA_06/02_AUT_EST_ENTREGA/public/js/deletePessoa.js
@@ -0,0 +1,90 @@
+import * as bootstrap from 'bootstrap'
+
+form.addEventListener("submit", () => {
+    // METODO FETCH
+    //
+    // fetch(`/api/delete/${id.value}`, {
+    //     method: "DELETE",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // }).then((res) => {
+    //     return res.json();
+    // }).then((data) => {
+    //     if (data.status && data.text) {
+    //         if (document.getElementById('remove_button')) {
+    //             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast'))
+    //             if (data.status === "success") {
+    //                 document.getElementById('toast-title').innerHTML = "Sucesso!"
+    //                 document.getElementById('toast-text').innerHTML = data.text
+    //                 document.getElementById('toast-img').src="./assets/check.svg"
+    //             }
+    //             else if (data.status === "error") {
+    //                 document.getElementById('toast-title').innerHTML = "Erro."
+    //                 document.getElementById('toast-text').innerHTML = data.text
+    //                 document.getElementById('toast-img').src="./assets/wrong.svg"
+    //             }
+    //             toastBootstrap.show()
+    //         }
+    //     }
+    // }).catch((err) => {
+    //     console.log(err);
+    // });
+
+
+    // METODO AJAX
+    //
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.open("DELETE", `/api/delete/${id.value}`, true);
+    // xhttp.setRequestHeader("Content-Type", "application/json");
+    // xhttp.send();
+    // xhttp.onreadystatechange = function () {
+    //     if (this.readyState == 4) {
+    //         var data = JSON.parse(this.responseText);
+    //         if (data.status && data.text) {
+    //             if (document.getElementById('remove_button')) {
+    //                 const toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast'))
+    //                 if (data.status === "success") {
+    //                     document.getElementById('toast-title').innerHTML = "Sucesso!"
+    //                     document.getElementById('toast-text').innerHTML = data.text
+    //                     document.getElementById('toast-img').src="./assets/check.svg"
+    //                 }
+    //                 else if (data.status === "error") {
+    //                     document.getElementById('toast-title').innerHTML = "Erro."
+    //                     document.getElementById('toast-text').innerHTML = data.text
+    //                     document.getElementById('toast-img').src="./assets/wrong.svg"
+    //                 }
+    //                 toastBootstrap.show()
+    //             }
+    //         }
+    //     }
+    // }
+
+
+    // METODO JQUERY
+
+    $.ajax({
+        url: `/api/delete/${id.value}`,
+        type: "DELETE",
+        contentType: "application/json",
+        success: function (data) {
+            if (data.status && data.text) {
+                if (document.getElementById('remove_button')) {
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast'))
+                    if (data.status === "success") {
+                        document.getElementById('toast-title').innerHTML = "Sucesso!"
+                        document.getElementById('toast-text').innerHTML = data.text
+                        document.getElementById('toast-img').src="./assets/check.svg"
+                    }
+                    else if (data.status === "error") {
+                        document.getElementById('toast-title').innerHTML = "Erro."
+                        document.getElementById('toast-text').innerHTML = data.text
+                        document.getElementById('toast-img').src="./assets/wrong.svg"
+                    }
+                    toastBootstrap.show()
+                }
+            }
+        }
+    });
+
+});
